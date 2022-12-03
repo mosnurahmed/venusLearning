0a9a495a34b9a3d18353dad2dfa9e5ddeff755ec
Mosnur Ahmed <mosnurahmed01@gmail.com>
2022-12-03 09:08:35 +0600

    first

---
+++ wp-content/plugins/tutor/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'themeum/tutor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8d016ba24c95fb2acdbe197a766ec82bb5eca0bd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'themeum/tutor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8d016ba24c95fb2acdbe197a766ec82bb5eca0bd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
